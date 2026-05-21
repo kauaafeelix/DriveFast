3c3d64bd01474e7524572f17744b2046abd26ee4
Kaua Felix <kfelixcosta11@gmail.com>
2026-05-21 10:43:18 -0300

    feat: inicia estudo para prova

---
+++ src/main/java/com/weg/centroweg/drive_fast/DriveFastApplication.java
@@ -0,0 +1,13 @@
+package com.weg.centroweg.drive_fast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriveFastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriveFastApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/weg/centroweg/drive_fast/DriveFastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weg.centroweg.drive_fast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriveFastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
